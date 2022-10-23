5a7433c6572a9d8f92f5e42396ad466d4f7c0258
Matheus <matheus_cs.nf@hotmail.com>
2022-10-23 01:29:49 -0300

    adicionando restriçoes a rota post /matches

--- app/backend/src/useCases/MatcheUseCase/createMatches/createMatche.service.ts
+++ app/backend/src/useCases/MatcheUseCase/createMatches/createMatche.service.ts
@@ -2,6 +2,7 @@ import CustomError from '../../../Error/customError';
 import { IMatchesTeam } from '../../../entities/IMatche';
 import SequelizeMatchesRepository from
   '../../../repositories/implementations/SequelizeMatches.repository';
+import Teams from '../../../database/models/Teams';
 
 export default class CreateMatcheService {
   private _matcheRepository: SequelizeMatchesRepository;
@@ -14,6 +15,12 @@ export default class CreateMatcheService {
     const { homeTeam, awayTeam } = infoMatches;
     if (homeTeam === awayTeam) {
       throw new CustomError('It is not possible to create a match with two equal teams', 422);
+    }
+    const teams = await Teams.findAll({ raw: true });
+    const homeTeamExist = teams.some((team) => team.id === homeTeam);
+    const awayTeamExist = teams.some((team) => team.id === homeTeam);
+    if (!homeTeamExist || !awayTeamExist) {
+      throw new CustomError('There is no team with such id!', 404);
     }
     const matche = await this._matcheRepository.create(infoMatches);
     return matche;
